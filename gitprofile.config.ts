f7a7028b5d7193a05b64f62b8bfe088181ea2cfe
M.A.S Hunter <108648096+mashunterbd@users.noreply.github.com>
2024-03-06 07:37:25 +0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -211,7 +211,7 @@ const CONFIG = {
       class="text-primary" href="https://github.com/mashunterbd/mashunter"
       target="_blank"
       rel="noreferrer"
-    >M.A.S Hunter</a> and ,
+    >mashunterbd</a>
 
   enablePWA: false,
 };
